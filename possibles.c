4704abe2cc8bc1b32f59d8c48a3f087277b33c1f
Bejoy Mathew <bejoygm@gmail.com>
2017-02-25 21:07:27 +0530

    upto repl builder

---
+++ possibles.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+// structures are declared to declare new types.
+// should be above any function that wishes to use it
+typedef struct {
+    float x;
+    float y;
+} point;
+
+int main() {
+    int count = 10;
+    int sum;
+    sum = add_together(1, 2);
+    printf("%d", sum);
+    point p;
+    p.x = 0.1;
+    p.y = 2;
+
+    float length = math.sqrt(p.x * p.x + p.y * p.y);
+}
+
+// functions 
+
+int add_together(int x, int y) {
+    int result = x + y;
+    return result;
+}
+
+
+
+
+
+
