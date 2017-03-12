2ea8ac98c4aface490c7550de28116d6c7cf33b9
Bejoy Mathew <bejoygm@gmail.com>
2017-03-12 19:57:15 +0530

    added history

--- repl_builder.c
+++ repl_builder.c
@@ -1,4 +1,8 @@
 # include <stdio.h>
+# include <stdlib.h>
+
+#include <editline/readline.h>
+#include <editline/history.h>
 
 /* Declare a buffer for user input of size 2048 */
 
@@ -12,15 +16,16 @@ int main(int argc, char** argv) {
 
     /* In a never ending loop */
     while (1) { 
-        /* Output prompt */
+        /* Output prompt and get input */
+        char* input = readline("lispy> ");
 
-        fputs("lispy> ", stdout);
-
-        /* Read a line of user input of maximum size 2048 */
-        fgets(input, 2048, stdin);
+        /* Add input to history */
+        add_history(input);
 
         /* Echo input back to user */
-        printf("No you're a %s", input);
+        printf("No you're a %s\n", input);
+
+        free(input);    
     }
 
     return 0;
